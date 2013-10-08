bd0aca43948569f1c6c2664418434b328209d02f
Slater-Victoroff <slater.r.victoroff@gmail.com>
2013-10-08 09:52:07 -0400

    fixed directory layout

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import os
+
+tmpl_dir = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(__name__, template_folder=tmpl_dir)
+app.config['DEBUG'] = True
+
+@app.route('/')
+def display():
+    return render_template('main.html')
+
+@app.route('/bubble')
+def bubble():
+    return render_template('bubble.html')
+
+@app.route('/bubble2')
+def bubble2():
+    return render_template('bubble2.html')
+
+@app.route('/graph')
+def graph():
+    return render_template("graph.html")
+
+if __name__ == '__main__':
+    app.run()
